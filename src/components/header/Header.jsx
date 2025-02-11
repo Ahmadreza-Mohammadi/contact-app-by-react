fe30c2b16c253333c3ed4b884e838fc14daf3573
noway777 <ahmadrezamohammadi777@gmail.com>
2025-02-11 18:19:36 +0330

    fix: some header styles changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,10 @@ function Header() {
   return (
     <>
       <div className="bg-gray-500 flex justify-end p-2 rounded-b-4xl">
-        <div className="w-[720px] flex justify-between items-center">
-          <h1 className="font-bold text-white t">وب اپلیکیشن مدیریت مخاطبین</h1>
+        <div className="w-1/2 flex justify-between ml-16">
+          <h1 className="text-center font-extrabold text-white">وب اپلیکیشن مدیریت مخاطبین</h1>
           <img
-            className="h-5 ml-2"
+            className="h-5"
             src="https://www.svgrepo.com/show/433086/light-mode.svg"
             alt=""
           />
